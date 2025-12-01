0306290b1ce19e4f8f0a5e5f776be085e1713802
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-12-01 18:36:37 +0800

    chore: update

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,15 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  pnpm: true,
-})
+export default antfu(
+  {
+    pnpm: true,
+  },
+  {
+    files: ['test/fixtures/packager/bun/package.json'],
+    rules: {
+      'jsonc/comma-dangle': ['warn', 'always'],
+      'jsonc/no-comments': 'off',
+    },
+  },
+)
